ca36e1ab9b0948f84a2b57e36e8118e03fd20af4
Tolu Emoruwa <tolu@Tolus-MacBook-Air.local>
2025-01-18 17:11:11 -0500

    Displaying info for spots

--- src/MapToggle/LotDetails.js
+++ src/MapToggle/LotDetails.js
@@ -1,4 +1,5 @@
-import './map-toggle.css';
+import './lot-details.css';
+
 import { useState } from 'react';
 
 function LotDetails({spot}) {
@@ -10,7 +11,28 @@ function LotDetails({spot}) {
                     
                 </div>
                 <div className='star-rating'>
-                    
+                    <div>
+                        Review:
+                    </div>
+                    <div>
+                        {spot.RATING}
+                    </div>
+                </div>
+            </div>
+            <div className='right-side'>
+                <div className='parking-details'>
+                    <div className='park-name'>{spot.ADDRESS}</div>
+                    <div className='price-and-available'>
+                        <div className='available-num'>{spot.AVAILABLE_SPACES}</div>
+                        <div className='price-num'>{spot.HOURLY_RATE}</div>
+                    </div>
+                    <div className='indoor-offcampus'>
+                        <div className='indoor-outdoor'>{spot.CATEGORY}</div>
+                        <div className='offcampus'>{spot.LOCATION}</div>
+                    </div>
+                </div>
+                <div className='bookmark-slab'>
+                    <div className='bookmark-button'>Bookmark</div>
                 </div>
             </div>
         </div>
