658d713fceeea7d4e3a97ee5d89d0256bb04c9f3
Ali B <alibhangu1@outlook.com>
2025-01-18 14:34:08 -0500

    ui changes for button map

--- src/MapToggle/maptoggle.js
+++ src/MapToggle/maptoggle.js
@@ -2,18 +2,24 @@ import Navbar from '../Navbar/navbar';
 import { Link } from "react-router-dom";
 import ParkingMap from '../MapToggle/map';
 import './map-toggle.css';
+import ChevronIcon from './chevron-back-outline.svg'
 
 function MapToggle() {
   return (
     <div className="map-body">
         <div className='map-nav-tools'>
-            <Link to={`/home`}>{`<`}</Link>
+            <Link to={`/home`}>
+              <img src={ChevronIcon} className='chevronIcon'/>
+            </Link>
+
         </div>
         <ParkingMap />   
         <div className='map-filters'>
 
         </div>
     </div>
+
+    
   );
 }
 
