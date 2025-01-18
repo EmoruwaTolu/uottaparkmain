79196a400a1a013af5a3e9f12eedc3e581ef85b7
Oscar Li <lioscar58@gmail.com>
2025-01-18 18:00:23 -0500

    fixed styles

--- src/MapToggle/LotDetails.js
+++ src/MapToggle/LotDetails.js
@@ -2,38 +2,40 @@ import './lot-details.css';
 
 import { useState } from 'react';
 
-function LotDetails({spot}) {
-    console.log(spot)
+function LotDetails({ spot }) {
+    console.log(spot);
     return (
         <div className='spot-container'>
             <div className='left-side'>
-                <div className='img-container'>
-                    
-                </div>
+                <div className='img-container'></div>
                 <div className='star-rating'>
-                    <div>
-                        Review:
-                    </div>
-                    <div>
-                        {spot.RATING}
-                    </div>
+                    <div>Review:</div>
+                    <div>{spot.RATING}</div>
                 </div>
             </div>
             <div className='right-side'>
-                <div className='parking-details'>
-                    <div className='park-name'>{spot.ADDRESS}</div>
-                    <div className='price-and-available'>
-                        <div className='available-num'>{spot.AVAILABLE_SPACES}</div>
-                        <div className='price-num'>{spot.HOURLY_RATE}</div>
+                <>
+                    <div className='parking-details'>
+                        <div className='park-name'>{spot.ADDRESS}</div>
+                        <div className='price-and-available'>
+                            <div className='available-num'>
+                                <div>Available:</div>
+                                <div>{spot.AVAILABLE_SPACES}</div>
+                            </div>
+                            <div className='price-num'>
+                                <div>Hourly Rate:</div>
+                                <div>{spot.HOURLY_RATE}</div>
+                            </div>
+                        </div>
+                        <div className='indoor-offcampus'>
+                            <div className='indoor-outdoor'>{spot.CATEGORY}</div>
+                            <div className='offcampus'>{spot.LOCATION}</div>
+                        </div>
                     </div>
-                    <div className='indoor-offcampus'>
-                        <div className='indoor-outdoor'>{spot.CATEGORY}</div>
-                        <div className='offcampus'>{spot.LOCATION}</div>
+                    <div className='bookmark-slab'>
+                        <div className='bookmark-button'>Bookmark</div>
                     </div>
-                </div>
-                <div className='bookmark-slab'>
-                    <div className='bookmark-button'>Bookmark</div>
-                </div>
+                </>
             </div>
         </div>
     );
